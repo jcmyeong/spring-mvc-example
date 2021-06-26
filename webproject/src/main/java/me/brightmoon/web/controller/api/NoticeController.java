e365cd5232fa70b4d55a6d289416a6d9c378cc5d
jcmeong <jcmyeong777@gmail.com>
2021-06-26 17:39:53 +0900

    Spring MVC (스프링 웹 MVC) 강의 32 - @RestController와 한글출력 설정 - https://www.youtube.com/watch?v=aDjJXbv14LU&t=416s

---
+++ webproject/src/main/java/me/brightmoon/web/controller/api/NoticeController.java
@@ -0,0 +1,16 @@
+package me.brightmoon.web.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("apiNoticeController")
+@RequestMapping("/api/notice/")
+public class NoticeController {
+
+	@RequestMapping("list")
+	public String list() {
+		
+		return "공지사항 list";
+	} // NoticeController noticeController = new NoticeController
+	  // <bean id="noticeController" class=".....NoticeController" >
+}
