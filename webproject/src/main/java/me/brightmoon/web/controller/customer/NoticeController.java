0ff0ca6810d6ae5977ec617929584b1a8ea2d503
jcmeong <jcmyeong777@gmail.com>
2021-06-26 18:13:06 +0900

    Spring MVC (스프링 웹 MVC) 강의 36 - QueryString 입력 #2 : 변수명 별칭과 기본 값처리 - https://www.youtube.com/watch?v=aBTbtFLScC0

--- webproject/src/main/java/me/brightmoon/web/controller/customer/NoticeController.java
+++ webproject/src/main/java/me/brightmoon/web/controller/customer/NoticeController.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import me.brightmoon.web.entity.Notice;
 import me.brightmoon.web.service.NoticeService;
 
 @Controller
@@ -20,11 +20,11 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("list")
-	public String list(String p) throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name ="p", defaultValue = "1") int page) throws ClassNotFoundException, SQLException {
 		//String p = request.getParameter("p");
-		System.out.println("page:" + p);
+		System.out.println("page:" + page);
 		
-		List<Notice> list = noticeService.getList(1, "TITLE", "");
+		//List<Notice> list = noticeService.getList(1, "TITLE", "");
 		
 		return "notice.list";
 	}
