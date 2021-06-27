c9329cb0144043fc4dbcd12cb77c56f6d9ff3887
jcmyeong88 <jcmyeong777@gmail.com>
2021-06-27 23:41:17 +0900

    Spring MVC (스프링 웹 MVC) 강의 40 - POST 입력 #2 (콤보박스 값 입력) - https://youtu.be/9QAAVeNmuJM

--- webproject/src/main/java/me/brightmoon/web/controller/admin/board/NoticeController.java
+++ webproject/src/main/java/me/brightmoon/web/controller/admin/board/NoticeController.java
@@ -15,9 +15,9 @@ public class NoticeController {	// <bean name="adminNoticeController" class="me.
 	
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content) {
+	public String reg(String title, String content, String category) {
 		
-		return String.format("title:%s<br>content:%s<br>", title, content);
+		return String.format("title:%s<br>content:%s<br>category:%s", title, content, category);
 	}
 	
 	@RequestMapping("edit")
